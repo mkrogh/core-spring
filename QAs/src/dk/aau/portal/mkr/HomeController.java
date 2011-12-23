721ff819f4b20a1526b5d6979a9e663c79042a07
Markus Krogh <markus@casadelkrogh.dk>
2011-12-23 10:13:21 +0100

    Intitial commit - a working "landing app"

---
+++ QAs/src/dk/aau/portal/mkr/HomeController.java
@@ -0,0 +1,28 @@
+package dk.aau.portal.mkr;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(Locale locale, Model model){
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		
+		return "home";
+	}
+	
+}
